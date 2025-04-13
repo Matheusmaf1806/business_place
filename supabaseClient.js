b8f863104d9b3909ea37d48a1c7436ace0990926
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-04-13 13:36:31 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,9 @@
 // supabaseClient.js
-import { createClient } from "https://jspm.dev/@supabase/supabase-js";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl =
-  process.env.SUPABASE_URL || "https://qiblwjpijwhtlmylrlxl.supabase.co";
+// Em ambiente de navegador, não há process.env, então usamos valores padrão:
+const supabaseUrl = "https://qiblwjpijwhtlmylrlxl.supabase.co";
 const supabaseAnonKey =
-  process.env.SUPABASE_ANON_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYmx3anBpandodGxteWxybHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4Nzk2MzcsImV4cCI6MjA1NzQ1NTYzN30.WyNSVoxb3y0-2wJooTJUsqPdzCpkpGjb3Xr2YyDazVQ";
 
 const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
