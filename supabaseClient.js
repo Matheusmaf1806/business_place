7d6cf6cea257d63a3bfd76a8bf0bd6f35493a858
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-04-13 13:00:29 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,7 @@
-// Supondo que suas variáveis de ambiente estejam configuradas corretamente no ambiente Node
-const { createClient } = require('@supabase/supabase-js');
+// supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; // ou outra variável, ex: SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; // ou SUPABASE_ANON_KEY
+const supabaseUrl = process.env.SUPABASE_URL || "https://qiblwjpijwhtlmylrlxl.supabase.co";
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYmx3anBpandodGxteWxybHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4Nzk2MzcsImV4cCI6MjA1NzQ1NTYzN30.WyNSVoxb3y0-2wJooTJUsqPdzCpkpGjb3Xr2YyDazVQ";
 
-const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-module.exports = supabaseClient;
+export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
