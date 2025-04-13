d05bb9dea43933675211430c3ece73532f67c11e
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-04-13 13:19:08 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
-// Use uma URL de CDN que forneça o pacote em ESM
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// supabaseClient.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = process.env.SUPABASE_URL || "https://qiblwjpijwhtlmylrlxl.supabase.co";
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYmx3anBpandodGxteWxybHhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE4Nzk2MzcsImV4cCI6MjA1NzQ1NTYzN30.WyNSVoxb3y0-2wJooTJUsqPdzCpkpGjb3Xr2YyDazVQ";
